a609fac158538a2a8346be91bc70b692409774d1
federojasdr <federico.rojas@devrev.ai>
2024-05-05 14:29:29 -0300

    Update queue_screen.dart

--- smart_pager/lib/screens/queue_screen.dart
+++ smart_pager/lib/screens/queue_screen.dart
@@ -13,96 +13,89 @@ class QueueScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () => GoRouter.of(context).pop(),
-            icon: const Icon(
-              Icons.arrow_back,
-              color: SPColors.activeBlack,
-              size: 30,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => GoRouter.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: SPColors.activeBlack,
+            size: 30,
           ),
-          title: const CustomText(
-            text: 'El Bulli',
-            fontSize: 35,
+        ),
+        title: const CustomText(
+          text: 'El Bulli',
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const SizedBox(height: 20),
+        Center(
+          // Wrap the Image.asset with Center widget
+          child: Image.asset(
+            'assets/images/black_logo.png', // Path to your restaurant image asset
+            width: 200, // Adjust size as needed
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(height: 15),
+        const Flexible(
+          child: CustomText(
+            text: 'Tiempo estimado de espera:',
+            color: SPColors.activeBlack,
+            fontSize: 20,
+            overflow: TextOverflow.ellipsis,
             fontWeight: FontWeight.bold,
           ),
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const SizedBox(height: 20),
-              Center(
-                // Wrap the Image.asset with Center widget
-                child: Image.asset(
-                  'assets/images/black_logo.png', // Path to your restaurant image asset
-                  width: 200, // Adjust size as needed
-                  height: 200,
-                  fit: BoxFit.cover,
-                ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.access_time,
+              color: SPColors.activeBlack,
+              size: 18,
+            ),
+            Flexible(
+              child: CustomText(
+                text: ' 20 minutos',
+                color: SPColors.darkGray,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 15),
-              const Flexible(
-                child: CustomText(
-                  text: 'Tiempo estimado de espera:',
-                  color: SPColors.activeBlack,
-                  fontSize: 20,
-                  overflow: TextOverflow.ellipsis,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.access_time,
-                    color: SPColors.activeBlack,
-                    size: 18,
-                  ),
-                  Flexible(
-                    child: CustomText(
-                      text: ' 20 minutos',
-                      color: SPColors.darkGray,
-                      fontSize: 20,
-                      overflow: TextOverflow.ellipsis,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(child: Container()),
-              const Flexible(
-                child: CustomText(
-                  text: 'Indicá cuantos van a ser',
-                  color: SPColors.activeBlack,
-                  fontSize: 20,
-                  overflow: TextOverflow.ellipsis,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const DropdownMenuExample(),
-              Expanded(child: Container()),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                    horizontal: 20), // Add left and right margin here
-                child: GradientButton(
-                  icon: Icons.wb_twilight,
-                  text: "Anotarse en la cola",
-                  gradientColors: const [
-                    SPColors.secondary,
-                    SPColors.secondary
-                  ],
-                  onPressed: () {
-                    //
-                  },
-                ),
-              ),
-              const SizedBox(height: 30),
-            ]),
-      ),
+            ),
+          ],
+        ),
+        Expanded(child: Container()),
+        const Flexible(
+          child: CustomText(
+            text: 'Indicá cuantos van a ser',
+            color: SPColors.activeBlack,
+            fontSize: 20,
+            overflow: TextOverflow.ellipsis,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const DropdownMenuExample(),
+        Expanded(child: Container()),
+        Container(
+          margin: const EdgeInsets.symmetric(
+              horizontal: 20), // Add left and right margin here
+          child: GradientButton(
+            icon: Icons.wb_twilight,
+            text: "Anotarse en la cola",
+            gradientColors: const [SPColors.secondary, SPColors.secondary],
+            onPressed: () {
+              //
+            },
+          ),
+        ),
+        const SizedBox(height: 30),
+      ]),
     );
   }
 }
