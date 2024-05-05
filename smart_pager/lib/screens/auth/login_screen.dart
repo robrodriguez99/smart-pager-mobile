1ff47cd3b55b9ef7fbb6201d5f86ed6a16f19288
federojasdr <federico.rojas@devrev.ai>
2024-05-05 13:31:53 -0300

    better login

--- smart_pager/lib/screens/auth/login_screen.dart
+++ smart_pager/lib/screens/auth/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: SPColors.primary,
       body: SingleChildScrollView(
         child: Center(
           child: SizedBox(
@@ -18,35 +19,64 @@ class LoginScreen extends StatelessWidget {
               children: [
                 const SizedBox(height: 20),
                 Image.asset(
-                  'assets/images/logo.png',
-                  width: 100,
-                  height: 100,
-                ),
-                const SizedBox(height: 20),
-                const Text('TODO: OAUTH GOOGLE',
-                    style: TextStyle(fontSize: 30)),
-                const SizedBox(height: 20),
-                GradientButton(
-                  text: 'Iniciar Sesión',
-                  onPressed: () {
-                    GoRouter.of(context).go('/home');
-                  },
-                  gradientColors: const [SPColors.primary, SPColors.primary],
+                  'assets/images/white_logo.png',
                   width: 200,
-                  height: 50,
-                  borderRadius: BorderRadius.circular(10),
+                  height: 200,
                 ),
-                const SizedBox(height: 20),
-                GradientButton(
-                  text: 'Registrarse',
-                  onPressed: () {
-                    GoRouter.of(context).go('/home');
-                  },
-                  gradientColors: const [SPColors.primary, SPColors.primary],
-                  width: 200,
-                  height: 50,
-                  borderRadius: BorderRadius.circular(10),
-                )
+                const SizedBox(height: 10), // Adjust spacing as needed
+                const Text(
+                  'Smart Pager',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      fontFamily: 'Averta'),
+                ),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.all(8.0), // Add padding
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).go('/home');
+                    },
+                    child: Container(
+                      width: 300,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: const LinearGradient(
+                          colors: [
+                            SPColors.primary,
+                            SPColors.primary,
+                          ],
+                        ),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3.0, // Increase border width
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/google.png',
+                            width: 30, // Adjust size as needed
+                            height: 30, // Adjust size as needed
+                          ),
+                          const SizedBox(width: 10),
+                          const Text(
+                            'Continuar con Google',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
