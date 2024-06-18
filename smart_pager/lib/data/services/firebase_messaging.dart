6e9b3e5aac1c36603b82e31f0d4f5197df5e24bf
robrodriguez99 <robrodriguez@itba.edu.ar>
2024-06-18 13:48:26 -0300

    chore: added foreground message management

--- smart_pager/lib/data/services/firebase_messaging.dart
+++ smart_pager/lib/data/services/firebase_messaging.dart
@@ -3,7 +3,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print('Handling a background message ${message.messageId}');
-  print(message.data);
+  print(message.notification?.title);
 }
 
 class FirebaseMessagingApi {
@@ -15,8 +15,47 @@ class FirebaseMessagingApi {
     await _firebaseMessaging.requestPermission();
     final token = await _firebaseMessaging.getToken();
     print('FirebaseMessaging token: $token');
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    initPushNotifications();
+    
   }
+
+
+  Future initPushNotifications() async {
+    await _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    
+    handleForegroundMessage();
+
+
+  }
+
+  void handleForegroundMessage() {
+    FirebaseMessaging.onMessage.listen((message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.notification?.title}');
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+  
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) {
+      print('No message');
+      return;
+    }
+    print('A new onMessage event was published');
+    print(message.category);
+  }
+
+
 
 
 
